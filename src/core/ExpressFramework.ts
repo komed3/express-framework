ccca4e2dab2a9e4a85fa1cb933767f6139b4ef56
komed3 <pk97@outlook.de>
2025-12-09 23:39:59 +0100

    install express

--- src/core/ExpressFramework.ts
+++ src/core/ExpressFramework.ts
@@ -1,3 +1,12 @@
 import { IFramework } from '../interfaces/IFramework';
+import express, { Express } from 'express';
 
-export class ExpressFramework implements IFramework {}
+export class ExpressFramework implements IFramework {
+
+    private app: Express;
+
+    constructor () {
+        this.app = express();
+    }
+
+}
