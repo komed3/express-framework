4c2c2e33d9bfe52a42c587aeb38bc08cd921c005
komed3 <pk97@outlook.de>
2025-12-10 09:40:25 +0100

    implement public methods

--- src/core/Logger.ts
+++ src/core/Logger.ts
@@ -1,5 +1,6 @@
 import { LoggingConfig } from '../types/config';
 import { ILogger } from '../types/interface';
+import { exit } from 'node:process';
 
 export class Logger implements ILogger {
 
@@ -7,6 +8,29 @@ export class Logger implements ILogger {
 
     constructor ( config: LoggingConfig ) {
         this.config = config;
+    }
+
+    private log ( level: LoggingConfig[ 'level' ], message: string, meta?: any ) : void {}
+
+    error ( message: string, meta?: any ) : void {
+        this.log( 'error', message, meta );
+    }
+
+    exit ( message: string, meta?: any ) : void {
+        this.log( 'error', message, meta );
+        exit( 1 );
+    }
+
+    warn ( message: string, meta?: any ) : void {
+        this.log( 'warn', message, meta );
+    }
+
+    info ( message: string, meta?: any ) : void {
+        this.log( 'info', message, meta );
+    }
+
+    debug ( message: string, meta?: any ) : void {
+        this.log( 'debug', message, meta );
     }
 
 }
